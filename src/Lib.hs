61ed1d19b2aef82d6719fa7709e53c63f20efa9f
arn25-lk <arnavb25@gmail.com>
2022-11-28 15:31:49 -0500

    define state

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,25 @@ module Lib
     ( someFunc
     ) where
 
-someFunc :: String
-someFunc = "Hello CIS 5520"
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+type Node = Int
+-- data Graph = G Vertices Edges deriving Show
+-- newtype Vertices = V [Node]
+data Edge = E {source::Node, dest:: Node, weight::Int}
+-- type Edges = [Edge]
+type GraphMap = Map Node [Edge]
+type Path = [Edge]
+
+toAdjList :: Graph -> GraphMap
+
+dijkstra :: GraphMap -> Node -> Map Node Path
+
+bfs :: GraphMap -> Node -> Map Node Path
+
+dfs :: GraphMap -> Node -> Map Node Path
+
+
+
+
