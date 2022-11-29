71891127be95e3597ebf5d65739fc26cfd071e54
arn25-lk <arnavb25@gmail.com>
2022-11-28 21:46:37 -0500

    definitions init

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,22 +4,58 @@ module Lib
 
 import Data.Map (Map)
 import qualified Data.Map as Map
+import qualified State as S
 
 type Node = Int
 -- data Graph = G Vertices Edges deriving Show
 -- newtype Vertices = V [Node]
-data Edge = E {source::Node, dest:: Node, weight::Int}
+data Edge = E {source::Node, dest:: Node, weight::Float}
 -- type Edges = [Edge]
 type GraphMap = Map Node [Edge]
 type Path = [Edge]
 
-toAdjList :: Graph -> GraphMap
+--toAdjList :: Graph -> GraphMap
+
+
+type Visited = S Store Bool 
+type Distances = S Store Bool 
+
+queryVisited :: (Store -> Bool) -> Visited
+queryVisited = undefined 
+
+queryDistance :: (Store -> Float) -> Distances
+queryDistance = undefined
+
+
+-- Non-negative graph map
+
 
 dijkstra :: GraphMap -> Node -> Map Node Path
+dijkstra = undefined
+    where 
+        search :: GraphMap -> Node -> Visited -> Distances
+        search = undefined
+        searchNeighbors :: GraphMap -> [Node] -> Visited -> Distances
+        searchNeighbors = undefined 
+
+
+AStar :: GraphMap -> Node -> Map Node Path
+AStar = undefined
+    where 
+        search :: GraphMap -> Node -> Visited -> Distances
+        search = undefined
+        searchNeighbors :: GraphMap -> [Node] -> Visited -> Distances
+        searchNeighbors = undefined 
+    
+-- Dijkstra only takes weighted graph without cycle
 
 bfs :: GraphMap -> Node -> Map Node Path
 
+
+
 dfs :: GraphMap -> Node -> Map Node Path
+
+
 
 
 
