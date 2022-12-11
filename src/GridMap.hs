3f867acff0de5cbacf3dab6c75789c3d7206e263
Alex Feng <ahfeng@seas.upenn.edu>
2022-12-11 17:50:11 -0500

    moved neigbors to Grid, added tests

--- src/GridMap.hs
+++ src/GridMap.hs
@@ -16,14 +16,22 @@ import qualified Brick.Widgets.Border as B
 import qualified Brick.Widgets.Border.Style as BS
 import qualified Brick.Widgets.Center as C
 import qualified Brick.Util as U
-
+import Test.QuickCheck
+  (Arbitrary (..),
+  Gen,
+  Property,
+  Testable (..),
+  (==>),
+  )
+import qualified Test.QuickCheck as QC
 
 -- | Identifier for tiles in a grid
 type Place = Int
--- | A map tile with an elevation and whether it is traversible
-data Tile = Tile { place :: Place, elevation :: Float, traversible :: Bool, neighbors :: [Place] } deriving (Show, Eq)
+-- | A map tile with an elevation, whether it is traversible
+data Tile = Tile { place :: Place, elevation :: Float, traversible :: Bool} deriving (Show, Eq)
 
-data Grid = Grid {rows :: Int, cols :: Int, tiles :: [Tile]} deriving (Show, Eq)
+-- | A grid of tiles with a given number of rows and columns, 
+data Grid = Grid {rows :: Int, cols :: Int, tiles :: [Tile], neighbors :: Map Place [Place]} deriving (Show, Eq)
 
 -- | Get the tile at the given row and column in the grid
 getTile :: Grid -> Int -> Int -> Tile -- row <= rows, col <= cols
@@ -35,7 +43,7 @@ edge t1 t2 = E (place t1) (place t2) (abs (elevation t1 - elevation t2))
 
 -- | Turn a grid into a graph by iterating through the grid and creating edges between traversible neighbors with weight equal to the difference in elevation
 gridToGraph :: Grid -> GraphMap
-gridToGraph grid = foldl (\m t -> if traversible t then Map.insert (place t) (map (edge t) (filter traversible (map (\p -> getTile grid (p `div` cols grid) (p `mod` cols grid)) (neighbors t)))) m else m) Map.empty (tiles grid)
+gridToGraph grid = foldl (\m t -> if traversible t then Map.insert (place t) (map (edge t) (filter traversible (map (\p -> getTile grid (p `div` cols grid) (p `mod` cols grid)) (neighbors grid t)))) m else m) Map.empty (tiles grid)
 
 -- | Get the places of the tiles adjacent to the given tile
 adjacents :: Int -> Int -> Place -> [Place]
@@ -44,20 +52,30 @@ adjacents rows cols place = filter (\p -> p >= 0 && p < rows * cols) [place - co
 -- | Create a grid of traversible tiles with 0 elevation with the given dimensions
 simpleGrid :: Int -> Int -> Grid
 simpleGrid rows cols = Grid rows cols (map (\p -> Tile p 0 True (adjacents rows cols p)) [0..rows * cols - 1])
-        
+
+-- | Create a grid from a list of elevations and a list of traversibility
+gridFromList :: Int -> Int -> [Float] -> [Bool] -> Grid --TODO: check that the lists are the right length
+gridFromList rows cols elevations traversible = updateNeighbors $ Grid rows cols (map (\(p, e, t) -> Tile p e t (adjacents rows cols p)) (zip3 [0..rows * cols - 1] elevations traversible)) Map.empty
 
 -- | Get the new neighbors of a tile in the grid based on traversibility
 getNeighbors :: Grid -> Int -> Int -> [Place]
-getNeighbors grid row col = filter (\p -> traversible (getTile grid (p `div` cols grid) (p `mod` cols grid))) (adjacents (rows grid) (cols grid) (row * cols grid + col))
+getNeighbors grid row col = filter (\p -> traversible (getTil`e grid (p `div` cols grid) (p `mod` cols grid))) (adjacents (rows grid) (cols grid) (row * cols grid + col))
 
 -- | Set the neighbors of a tile in the grid
 setNeighbors :: Grid -> Int -> Int -> [Place] -> Grid
-setNeighbors grid row col newNeighbors = grid {tiles = map (\t -> if place t == place (getTile grid row col) then t {neighbors = newNeighbors} else t) (tiles grid)}
+setNeighbors grid row col newNeighbors = grid {neighbors = Map.insert (row * cols grid + col) newNeighbors (neighbors grid)}
 
--- | Set the traversibility of a tile in the grid
+-- | Update the neighbors of all tiles in the grid
+updateNeighbors :: Grid -> Grid
+updateNeighbors grid = grid {neighbors = Map.fromList (map (\t -> (place t, getNeighbors grid (place t `div` cols grid) (place t `mod` cols grid))) (tiles grid))}
+
+-- | Set the traversibility of a tile in the grid, updating the neighbors of the tile and its neighbors
 setTraversible :: Grid -> Int -> Int -> Bool -> Grid
-setTraversible grid row col newTraversible = grid {tiles = map (\t -> if place t == place (getTile grid row col) then t {traversible = newTraversible} else t) (tiles grid)}
-
+setTraversible grid row col newTraversible = grid {tiles = map (\t -> 
+  if place t == place (getTile grid row col) 
+    then t {traversible = newTraversible} 
+    else t) 
+    (tiles grid), neighbors = Map.insert (row * cols grid + col) (getNeighbors grid row col) (neighbors grid)}
 -- | Set the elevation of a tile in the grid
 setElevation :: Grid -> Int -> Int -> Float -> Grid
 setElevation grid row col newElevation = grid {tiles = map (\t -> if place t == place (getTile grid row col) then t {elevation = newElevation} else t) (tiles grid)}
@@ -84,3 +102,47 @@ gridTable grid = table (map (\r -> map (\c -> str (drawTile (getTile grid r c)))
 
 drawGrid :: Grid -> Widget ()
 drawGrid grid = C.center $ renderTable (gridTable grid)
+
+-- Property Testing
+
+-- | Check that the grid is the correct size
+prop_gridSize :: Grid -> Bool
+prop_gridSize grid = length (tiles grid) == rows grid * cols grid
+
+-- | Check that each tile has the right place
+prop_gridPlaces :: Grid -> Bool
+prop_gridPlaces grid = all (\t -> place t == place (getTile grid (place t `div` cols grid) (place t `mod` cols grid))) (tiles grid)
+
+-- | Check that the grid has the correct set of traversible neighbors for each tile
+prop_gridNeighbors :: Grid -> Bool
+prop_gridNeighbors grid = all (\t -> neighbors grid t == map (\p -> place (getTile grid (p `div` cols grid) (p `mod` cols grid))) (adjacents (rows grid) (cols grid) (place t))) (tiles grid)
+
+-- | Check that the grid has the correct set of traversible neighbors for each tile after setting the traversibility of a tile
+prop_gridNeighborsTraversible :: Grid -> Int -> Int -> Bool
+prop_gridNeighborsTraversible grid row col = all (\t -> neighbors grid t == map (\p -> place (getTile grid (p `div` cols grid) (p `mod` cols grid))) (adjacents (rows grid) (cols grid) (place t))) (tiles (setTraversible grid row col False))
+
+-- | Check that the graph produced by the grid has the correct number of nodes
+prop_gridGraphNodes :: Grid -> Bool
+prop_gridGraphNodes grid = length (gridToGraph grid) == rows grid * cols grid
+
+-- | Check that the graph produced by the grid has the correct number of edges
+prop_gridGraphEdges :: Grid -> Bool
+prop_gridGraphEdges grid = all (\t -> length (gridToGraph grid Map.! place t) == length (neighbors grid t)) (tiles grid)
+
+-- | Check that the graph produced by the grid has the correct edges
+prop_gridGraphEdges2 :: Grid -> Bool
+prop_gridGraphEdges2 grid = all (\t -> all (\e -> place e `elem` neighbors grid t) (gridToGraph grid Map.! place t)) (tiles grid)
+
+-- | Check that the graph produced by the grid has the correct weights
+prop_gridGraphWeights :: Grid -> Bool
+prop_gridGraphWeights grid = all (\t -> all (\e -> weight e == abs (elevation t - elevation (getTile grid (place e `div` cols grid) (place e `mod` cols grid)))) (gridToGraph grid Map.! place t)) (tiles grid)
+
+-- Arbitrary Instances
+
+instance Arbitrary Grid where
+  arbitrary = do
+    rows <- choose (1, 10)
+    cols <- choose (1, 10)
+    elevations <- vectorOf (rows * cols) (choose (0, 10))
+    traversible <- vectorOf (rows * cols) (elements [True, False])
+    return (gridFromList rows cols elevations traversible)
