b44adc4e3b21a852e2d6e69866eba311847c0ab6
Nono-Song <noeland@seas.upenn.edu>
2023-11-29 18:37:18 -0500

    Want to enforce moveFromSquare and deleted some confusing code

--- src/Parser.hs
+++ src/Parser.hs
@@ -18,21 +18,13 @@ data MoveType = NormalMove | KingsideCastling | QueensideCastling deriving (Show
 data Move = Move
   { moveType :: MoveType,
     piece :: Maybe Piece,
-    -- Disambiguation fields
-    -- Exactly one of the following two fields would have valid value
-    fromFile :: Maybe Char,
-    fromRank :: Maybe Int,
+    fromSquare :: Maybe Square, -- move from square
     toSquare :: Maybe Square, -- destination square
     promotion :: Maybe Piece,
     isCapture :: Bool,
     isCheck :: Bool,
     isCheckmate :: Bool
   }
-  deriving (Show, Eq)
-
-data Disambiguation
-  = FileDisambiguation Char
-  | RankDisambiguation Int
   deriving (Show, Eq)
 
 -- Given a space separated moves and return a list of Moves
@@ -47,30 +39,29 @@ test_SingleMove :: Test
 test_SingleMove =
   TestList
     [ -- Test Pawn move
-      fmap moveType (parseSingleMove "ee3") ~?= Right NormalMove,
-      fmap piece (parseSingleMove "ee3") ~?= Right (Just Pawn),
-      fmap fromFile (parseSingleMove "ee3") ~?= Right (Just 'e'),
-      fmap toSquare (parseSingleMove "ee3") ~?= Right (Just (Square 'e' 3)),
-      fmap isCheck (parseSingleMove "ee3") ~?= Right False,
-      fmap toSquare (parseSingleMove "ee3") ~?= Right (Just (Square 'e' 3)),
+      fmap moveType (parseSingleMove "e7e3") ~?= Right NormalMove,
+      fmap piece (parseSingleMove "e7e3") ~?= Right (Just Pawn),
+      fmap fromSquare (parseSingleMove "e7e3") ~?= Right (Just (Square 'e' 7)),
+      fmap toSquare (parseSingleMove "e7e3") ~?= Right (Just (Square 'e' 3)),
+      fmap isCheck (parseSingleMove "e7e3") ~?= Right False,
+      fmap toSquare (parseSingleMove "e7e3") ~?= Right (Just (Square 'e' 3)),
       -- Test King move
-      fmap moveType (parseSingleMove "Kab4") ~?= Right NormalMove,
-      fmap piece (parseSingleMove "Kab4") ~?= Right (Just King),
-      fmap fromFile (parseSingleMove "Kab4") ~?= Right (Just 'a'),
-      fmap toSquare (parseSingleMove "Kab4") ~?= Right (Just (Square 'b' 4)),
+      fmap moveType (parseSingleMove "Ka1b4") ~?= Right NormalMove,
+      fmap piece (parseSingleMove "Ka1b4") ~?= Right (Just King),
+      fmap fromSquare (parseSingleMove "Ka1b4") ~?= Right (Just (Square 'a' 1)),
+      fmap toSquare (parseSingleMove "Ka1b4") ~?= Right (Just (Square 'b' 4)),
       -- Test Castling
       fmap moveType (parseSingleMove "O-O") ~?= Right KingsideCastling,
       fmap moveType (parseSingleMove "O-O-O") ~?= Right QueensideCastling,
       -- Test Pawn Promotion
-      fmap moveType (parseSingleMove "7a8=Q") ~?= Right NormalMove,
-      fmap piece (parseSingleMove "7a8=Q") ~?= Right (Just Pawn),
-      fmap fromFile (parseSingleMove "7a8=Q") ~?= Right Nothing,
-      fmap fromRank (parseSingleMove "7a8=Q") ~?= Right (Just 7),
-      fmap toSquare (parseSingleMove "7a8=Q") ~?= Right (Just (Square 'a' 8)),
-      fmap promotion (parseSingleMove "7a8=Q") ~?= Right (Just Queen),
-      fmap promotion (parseSingleMove "7a8=N") ~?= Right (Just Knight),
-      fmap promotion (parseSingleMove "7a8=R") ~?= Right (Just Rook),
-      fmap promotion (parseSingleMove "7a8=B") ~?= Right (Just Bishop)
+      fmap moveType (parseSingleMove "e7a8=Q") ~?= Right NormalMove,
+      fmap piece (parseSingleMove "e7a8=Q") ~?= Right (Just Pawn),
+      fmap fromSquare (parseSingleMove "e7a8=Q") ~?= Right Nothing,
+      fmap toSquare (parseSingleMove "e7a8=Q") ~?= Right (Just (Square 'a' 8)),
+      fmap promotion (parseSingleMove "e7a8=Q") ~?= Right (Just Queen),
+      fmap promotion (parseSingleMove "e7a8=N") ~?= Right (Just Knight),
+      fmap promotion (parseSingleMove "e7a8=R") ~?= Right (Just Rook),
+      fmap promotion (parseSingleMove "e7a8=B") ~?= Right (Just Bishop)
     ]
 
 -- Space separated moves parser
