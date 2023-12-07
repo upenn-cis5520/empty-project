2e3b97d63b3db6aa709673470e2a95fee26df079
aggarnav <aggarnav@gmail.com>
2023-12-06 19:22:43 -0500

    Formatting

--- src/ChessGame.hs
+++ src/ChessGame.hs
@@ -3,8 +3,8 @@ module ChessGame (playMove, playMoves, printGame) where
 import ChessParser
 import ChessSyntax
 import Control.Monad.State qualified as S
+import Data.Char (chr, ord)
 import Data.Map qualified as Map
-import Data.Char (ord, chr)
 
 -- Given a player, check if they are in check
 isCheck :: Move -> S.State Game Bool
@@ -30,34 +30,38 @@ playMove = undefined
 playMoves :: [Move] -> S.State Game ()
 playMoves = undefined
 
--- Print the current Game
+-- Print a Game's current state
 printGame :: S.State Game String
 printGame = do
   (Game b col) <- S.get
   return $ firstRow ++ "\n" ++ secondRow ++ "\n" ++ printRow 8 b ++ "\nIt is currently " ++ show col ++ "'s turn.\n"
 
 printRow :: Int -> Board -> String
-printRow r b = if r == 0 
-  then
-    secondRow ++ "\n" ++ firstRow 
-  else 
-    show r ++ " |" ++ printRow' r 'a' b ++ "| " 
-      ++ show r ++ "\n" ++ printRow (r - 1) b
+printRow r b =
+  if r == 0
+    then secondRow ++ "\n" ++ firstRow
+    else
+      show r
+        ++ " |"
+        ++ printRow' r 'a' b
+        ++ "| "
+        ++ show r
+        ++ "\n"
+        ++ printRow (r - 1) b
 
 printRow' :: Int -> Char -> Board -> String
 printRow' i c b =
-  if c == 'h' 
+  if c == 'h'
     then val
-  else val ++ " " ++ printRow' i (chr (ord c + 1)) b
+    else val ++ " " ++ printRow' i (chr (ord c + 1)) b
   where
     val :: String
     val = case Map.lookup (Square c i) b of
       Just x -> show x
       Nothing -> "x"
 
-
 firstRow :: String
 firstRow = "   a b c d e f g h   "
+
 secondRow :: String
 secondRow = "  +---------------+  "
-  
